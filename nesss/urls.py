49b02805790f9c79cd1452b473d2bf12af5ff793
Jessie <playlaugh2011@gmail.com>
2013-05-04 01:22:50 +0800

    1.1

---
+++ nesss/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'nesss.views.home', name='home'),
+    # url(r'^nesss/', include('nesss.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
